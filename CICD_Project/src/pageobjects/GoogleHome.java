e1c4e47669ae984a576d4b3f868ad0769df9760a
naman misra <naman.misra@provartesting.com>
2020-05-22 20:18:19 +0530

    Buildfile

--- CICD_Project/src/pageobjects/GoogleHome.java
+++ CICD_Project/src/pageobjects/GoogleHome.java
@@ -7,18 +7,15 @@ import org.openqa.selenium.support.FindBy;
 
 import com.provar.core.testapi.annotations.*;
 
-@Page( title="GoogleHome"                                
-     , summary=""
-     , relativeUrl=""
-     , connection="GoogleConnection"
-     )             
+@Page(title = "GoogleHome", summary = "", relativeUrl = "", connection = "GoogleConnection")
 public class GoogleHome {
 
 	@LinkType()
 	@FindBy(xpath = "//a[normalize-space(.)='Gmail']")
 	public WebElement gmail;
+
 	@TextType()
 	@FindBy(xpath = "//input[@aria-label='Search']")
-	public WebElement seatchBox;
-			
+	public WebElement searchBox;
+
 }
